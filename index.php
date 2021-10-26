93710b725bfd810ffcde1edfb8114f065ef7edb8
Digant Singh Khatkar <dsk1812@gmail.com>
2021-10-26 08:43:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
-echo "\n Hiiii... Hey Everyone I am running on this container".gethostname(); 
+echo "\n Hiiii... Hey Everyone I am running on this container".gethostname();
+echo "\n Hows it going?"
 echo "\n\n";
 ?>
